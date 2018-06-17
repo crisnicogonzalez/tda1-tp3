14ceb631fd396f53b27a93d95ca2340dad8b16e3
seguijoaquin <seguijoaquin@users.noreply.github.com>
2018-06-16 21:09:01 -0300

    Update dinamic_strategy_more_attack.py

---
+++ src/pseudocode/dinamic_strategy_more_attack.py
@@ -0,0 +1,13 @@
+/*
+ * Receives: Index of Damage Table representing a state in the game and the Damage Table
+ * Returns: Priority list of attacks for that specific state of the game
+ */
+
+def step(state, damage_table):
+   priority_list = []
+   if (state exists in KnownStates):
+       return KnownStates[state]
+   else
+       priority_list = order_by_max_damage(damage_table[state])
+       KnownStates[state] = priority_list
+   return priority_list
