cde71d7f588d75eb8d5ce969a72748f1375cc3c3
seguijoaquin <segui.joaquin@gmail.com>
2018-06-16 22:14:20 -0300

    dinamic strategy - part 1

---
+++ src/strategy/dinamic_strategy.py
@@ -0,0 +1,32 @@
+
+class DinamicStrategy():
+    def __init__(self,game,damage_table,shooters):
+        self.game = game
+        self.known_states = {}
+        self.damage_table = damage_table
+        self.shooters = shooters
+    
+    def step(self,state):
+        priority_list = []
+        if state in self.known_states:
+            return self.known_states[state]
+        else:
+            priority_list = self.order_by_max_damage(self.damage_table[state])
+            self.known_states[state] = priority_list
+        return priority_list
+
+    def order_by_max_damage(self,list):
+        return list
+    
+    def pop_first_from(self,list):
+        return list[0]
+
+    def execute(self):
+        index = self.game.get_current_column()
+        priority_list = self.step(index)
+        for i in range(self.shooters):
+            ship_position = priority_list.pop()
+            if self.game.ship_of_position_is_alive(ship_position):
+                self.game.attack_to_position(ship_position)
+
+            
